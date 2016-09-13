5b32fcbb1586a96b13d68ebca4cf5b2ab190596d
ianamsyi <ianamsyi19@gmail.com>
2016-09-12 18:32:30 -0700

    Add files via upload

---
+++ COBA_2_YG_KEDUA.ino
@@ -0,0 +1,36 @@
+void setup() {
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(A0,INPUT);
+  digitalWrite(A0,LOW);
+  pinMode(A1,INPUT);
+  digitalWrite(A1,HIGH);
+  pinMode(A2,INPUT);
+  digitalWrite(A2,HIGH);
+}
+
+void loop() 
+{
+  if (digitalRead(A0)== LOW)
+  {
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(9,HIGH);
+  delay(500);
+  digitalWrite(9,LOW);
+  delay(500);
+  digitalWrite(10,HIGH);
+  delay(500);
+  digitalWrite(10,LOW);
+  delay(500);
+  digitalWrite(11,HIGH);
+  delay(500);
+  digitalWrite(11,LOW);
+  delay(500);
+  }
+  else (digitalRead(A1)== HIGH);
+}
